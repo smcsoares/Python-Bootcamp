2f5b0c5986f4ea50924c69f98ab296d6536af204
Soares <smcsoares20@gmail.com>
2024-07-30 21:47:34 +0100

    string functions

--- Day2/main.py
+++ Day2/main.py
@@ -8,4 +8,26 @@ print(x[4])
 
 # Getting a particular element from a string -> sub-scripting
 
+print("123" + "456")
+
 # Integer
+
+print(123 + 345)
+
+#Large Integers
+print(123_456_789)
+
+#Float
+
+3.14159
+
+#Boolean 
+True
+False
+
+###################################
+num_char = len(input("What is your name?"))
+#print("Your name has " + num_char + " characters.")
+
+#print(type(num_char))
+print("Your name has " + str(num_char) + " characters.")
