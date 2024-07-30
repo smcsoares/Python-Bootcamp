f17963bb3ede9f44bdfb6da0f0a5567f6ecd4767
Soares <smcsoares20@gmail.com>
2024-07-30 21:47:00 +0100

    End day1

--- Day1/main.py
+++ Day1/main.py
@@ -1,30 +1,41 @@
-print("Hello world!")
+# print("Hello world!")
 
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print(\"what to print\")")
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print(\"what to print\")")
 
-print("Hello world!\nHello world!\nHello world!")
+# print("Hello world!\nHello world!\nHello world!")
 
-print("Hello" + " " + "Sérgio")
+# print("Hello" + " " + "Sérgio")
 
-# This is a comment
-print("Hello " + input("What is your name ?")+ "!")
+# # This is a comment
+# print("Hello " + input("What is your name ?")+ "!")
 
-x = input("what is your name ?")
-# We can only concatenate str (not "int")to str
-print("The number of letters in " + x + " is " + str(len(x)))
+# x = input("what is your name ?")
+# # We can only concatenate str (not "int")to str
+# print("The number of letters in " + x + " is " + str(len(x)))
 
-# Exercice
-# Change variables
-a = input()
-b = input()
+# # Exercice
+# # Change variables
+# a = input()
+# b = input()
 
-temp = a
-a = b
-b = temp
+# temp = a
+# a = b
+# b = temp
 
-print("a: " + a)
-print("b: " + b)
+# print("a: " + a)
+# print("b: " + b)
 
+#a = 123
+
+#print(type((float(a))))
+
+#Write a program that adds the digits in a 2 digit number e.g. if the input was 35, the the output should be 3+5 = 8
+
+# two_digit_number = input("Two digit number:")
+
+# sum = int(two_digit_number[0]) + int(two_digit_number[1])
+
+# print(sum)
 
